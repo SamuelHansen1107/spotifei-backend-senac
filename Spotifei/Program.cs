64be5a8ed36896cb40b1239bd436a4747da65eb8
Adriano Santos <adrianocsantos@gmail.com>
2025-09-01 21:13:20 -0300

    Update Program.cs

--- Spotifei/Program.cs
+++ Spotifei/Program.cs
@@ -1,3 +1,4 @@
 ﻿Console.WriteLine("Samuel Hansen");
 Console.WriteLine("Marcos Antonio da Silveira Ramos");
 Console.WriteLine("Isadora Padilha Garcia");
+Console.WriteLine("Adriano da Conceição Santos");
