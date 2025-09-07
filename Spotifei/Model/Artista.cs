a0f2e7318a6258a4b2b86ad53804698e815bad7c
Samuel Hansen <samuelhansen07@hotmail.com>
2025-09-07 13:40:41 -0300

    Classes adicionadas em View, Controller e DAL

---
+++ Spotifei/Model/Artista.cs
@@ -0,0 +1,16 @@
+public class Artista
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public string Genero { get; set; }
+
+    public Artista(string nome, string genero)
+    {
+        Nome = nome;
+        Genero = genero;
+    }
+    public Artista() // contr vazio 
+    {
+        
+    }
+}
---
+++ Spotifei/Model/Musica.cs
@@ -0,0 +1,22 @@
+public class Musica
+{
+    public int Id { get; set; }
+    public string Titulo { get; set; }
+    public TimeSpan Duracao { get; set; }
+    public Artista Artista { get; set; }
+    public Album Album { get; set; }
+    public int AlbumId { get; set; }
+
+    public Musica(string titulo, TimeSpan duracao, Artista artista, Album album)
+    {
+        Titulo = titulo;
+        Duracao = duracao;
+        Artista = artista;
+        Album = album;
+    }
+
+    public Musica()
+    {
+
+    }
+}
