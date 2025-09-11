fd4849135218fd0fb554daf395d5b946e82fc58f
Samuel Hansen <samuelhansen07@hotmail.com>
2025-09-11 20:07:55 -0300

    Criação dos Controllers e ajustes no MVC

---
+++ Spotifei/Controller/PlaylistController.cs
@@ -0,0 +1,38 @@
+class PlaylistController
+{
+    public bool VerificarSeExistePlaylist()
+    {
+        List<Playlist> playlists = new List<Playlist>();
+        if (playlists.Count != 0)
+        {
+            return true;
+        }
+        return false;
+    }
+    public void AdicionarPlaylist(Playlist playlist)
+    {
+        List<Playlist> playlists = new List<Playlist>();
+        playlists.Add(playlist);
+    }
+    public void RemoverPlaylist(Playlist playlist)
+    {
+        List<Playlist> playlists = new List<Playlist>();
+        playlists.Remove(playlist);
+    }
+    public void AtualizarPlaylist(Playlist playlist)
+    {
+        List<Playlist> playlists = new List<Playlist>();
+        playlists.Remove(playlist);
+        playlists.Add(playlist);
+    }
+    public void ListarTodasPlaylists()
+    {
+        PlaylistDAO playlistDAO = new PlaylistDAO();
+        playlistDAO.ListarTodos();
+    }
+    public void ListarPlaylist(int id)
+    {
+        PlaylistDAO playlistDAO = new PlaylistDAO();
+        playlistDAO.Listar(id);
+    }
+}
